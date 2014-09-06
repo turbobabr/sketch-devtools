01125c0d9cea45111881b1fcedc4402cf0a66c47
turbobabr <andrey.shakhmin@gmail.com>
2014-09-06 13:22:46 +0900

    Moved building of external framework to a relative location.

---
+++ client-src/frameworks/SketchConsole/Build/Products/Release/SketchConsole.framework/Versions/A/Headers/TBRConsoleViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBRConsoleViewController.h
+//  SketchConsole
+//
+//  Created by Andrey on 24/08/14.
+//  Copyright (c) 2014 Turbobabr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TBRConsoleViewController : NSViewController
+
+@end
